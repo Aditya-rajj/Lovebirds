e648ba607cb4b0b2e85d989e66c448a55111be5e
Aditya raj <149693721+Aditya-rajj@users.noreply.github.com>
2026-05-31 08:41:20 +0530

    Delete api directory

--- api/auth.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
-  
-  const { code } = req.body;
-  if (code === process.env.SECRET_KEY) {
-    return res.status(200).json({ success: true });
-  }
-  return res.status(401).json({ success: false });
-}
